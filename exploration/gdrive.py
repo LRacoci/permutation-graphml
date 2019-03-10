a0f258608cc108dfbc24537b6f08a9de517235bc
Lucas <racoci.0@gmail.com>
2019-03-09 22:27:46 -0300

    [exploration] Testing implementation of Saving and Reading with PyDrive

--- exploration/gdrive.py
+++ exploration/gdrive.py
@@ -1,52 +1,57 @@
-def getDrive():
-      # Install the PyDrive wrapper & import libraries.
-  # This only needs to be done once in a notebook.
-  !pip install -U -q PyDrive
-  from pydrive.auth import GoogleAuth
-  from pydrive.drive import GoogleDrive
-  from google.colab import auth
-  from oauth2client.client import GoogleCredentials
+import os
 
-  # Authenticate and create the PyDrive client.
-  # This only needs to be done once in a notebook.
-  auth.authenticate_user()
-  gauth = GoogleAuth()
-  gauth.credentials = GoogleCredentials.get_application_default()
-  return GoogleDrive(gauth)
+class GoogleDrive():
+  def __init__(self, path = "drive"):
+    # Install the PyDrive wrapper & import libraries.
+    # This only needs to be done once in a notebook.
+    !pip install -U -q PyDrive
+    from pydrive.auth import GoogleAuth
+    from pydrive.drive import GoogleDrive
+    from google.colab import auth
+    from oauth2client.client import GoogleCredentials
 
-def loadFile(parentId, name):
-  drive = getDrive()
-  return drive.ListFile({'q': "'{}' in parents and name contains '{}'".format(parentId, name)}).GetList()
-  
-def driveSave(contentString):
-  folder_id = "1czRhuoUEDIYo6JaqLSkuCjwdbUw6zbDW"
-  
-  drive = getDrive()
+    # Authenticate and create the PyDrive client.
+    # This only needs to be done once in a notebook.
+    auth.authenticate_user()
+    gauth = GoogleAuth()
+    gauth.credentials = GoogleCredentials.get_application_default()
+    self._drive = GoogleDrive(gauth)
+    self.path = path
+    if os.path.isdir(path):
+      print("{} already exists".format(path))
+    else:
+      os.mkdir(path)
 
-  # Create & upload a text file.
-  uploaded = drive.CreateFile({'title': 'Sample file.txt'})
-  uploaded.SetContentString(contentString)
-  uploaded.Upload()
-  print('Uploaded file with ID {}'.format(uploaded.get('id')))
-
-#driveSave('Sample upload file content')
-
-## Test 
-import json
-
-def graphListsGenerator(): 
-  drive= getDrive()
-  files = drive.ListFile({'q': "'{}' in parents".format("1s5tKKGpwkL7jSCqD96oiMMEnxeG0SIxN")})
-  files = files.GetList()
-  for file in files:
-    print("Reading file {}".format(file["title"]))
-    yield file.GetContentString()
+  def load(self, parentId, parent):
+    files = self._drive.ListFile({'q': "'{}' in parents".format(parentId)}).GetList()
+    if not parent:
+      parent = parentId
     
-def graphGenerator():
-  glg = graphListsGenerator()
-  for gl in glg:
-    for g6 in gl.split("\n"):
-      g = nx.from_graph6_bytes(g6)#str.encode(g6))
-      yield g
+    path = '/'.join([self.path, parent])
+    if not os.path.isdir(path):
+      os.mkdir(path)
     
-print(graphGenerator().next())
+    for file in files:
+      title = file['title']
+      path = '/'.join([self.path, parent, title])
+      print ("Saving {} in {}".format(title, path))
+      file.GetContentFile(path)
+  
+  def save(source = "results", target = "1Twv_oBTB_P9aQ7VWinMOwsLScj6N8Hfc"):
+    if not os.path.isdir(source):
+      print ("Please provide a valid path to save in google drive")
+    
+    for file in os.listdir(source):
+      if not os.path.isdir(file):
+        dFile = self._drive.CreateFile({'title': title, "parents": [{"id": target}]})
+        dFile.SetContentFile(file)
+        dFile.Upload()
+      # Create & upload a text file.
+      print('Uploaded file with ID {}'.format(uploaded.get('id')))
+      
+g = GoogleDrive()
+
+# Test
+
+g.load("1s5tKKGpwkL7jSCqD96oiMMEnxeG0SIxN")
+g.save("drive/1s5tKKGpwkL7jSCqD96oiMMEnxeG0SIxN")

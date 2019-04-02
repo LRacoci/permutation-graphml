29c6285b52113ddb10957c019f147efb21e777bc
Lucas Racoci <racoci.0@gmail.com>
2019-04-02 20:54:34 -0300

    [surface-exploration] Drawing darwin surface to vizualise examples

--- exploration/surface.py
+++ exploration/surface.py
@@ -47,28 +47,10 @@ SEED = 1
 np.random.seed(SEED)
 tf.set_random_seed(SEED)
 
-#@title Helper functions  { form-width: "30%" }
+#@title Helper functions  { form-width: "20%" }
 
 # pylint: disable=redefined-outer-name
 
-
-def darwin_batches_to_networkx_graphs(graphs, node_features):
-    '''
-    Args:
-        graph : adjacency matrix of the graph, shape = (num_graphs, num_nodes, num_nodes)
-        node_features :  Matrix of node features, shape = (num_graphs, num_nodes, num_node_features)
-    
-    Returns:
-        graphs_tuple : GraphTuple from graph_nets
-    '''
-    nxGraphs = []
-    for graph, node_feature in zip(graphs, node_features):
-        nxGraph = nx.from_numpy_matrix(graph, create_using=nx.DiGraph)
-        nx.set_node_attributes(G = nxGraph, name ="features", values = {n : val for n,val in enumerate(node_feature)})
-        nx.set_edge_attributes(G = nxGraph, name ="features", values = 0)
-        nxGraphs.append(nxGraph)
-    
-    return nxGraphs
 
 def print_graph(g):
     for s, t, w in g.edges(data=True):
@@ -104,52 +86,113 @@ def to_one_hot(indices, max_value, axis=-1):
 
 def create_feature(feature, fields):
     return np.hstack([np.array(feature[field], dtype=float) for field in fields])
-
     
-def generate_raw_graphs(
-	rand,
-    num_examples = 2,
-    min_max_nodes = 4,
-	geo_density = None
-):
-	types=[
-		'elliptic_paraboloid',
-		'saddle',
-		'torus',
-		'ellipsoid',
-		'elliptic_hyperboloid',
-		'another'
-	]
-	min_nodes, max_nodes = min_max_nodes
-	num_nodes = rand.randint(min_nodes, max_nodes)
-	
-	gen_graph = GenerateDataGraphSurface(type_dataset='elliptic_hyperboloid', num_surfaces=num_examples, num_points=num_nodes)
-	epochs=1
-	batch_size=10
-	for epoch in range(epochs):
-		print("\n########## epoch " + str(epoch+1) + " ##########")
-		gen_trainig = gen_graph.train_generator( batch_size = batch_size )
-		counter = 0
-		for gt_graph, set_feature, in_graph in gen_trainig:
-			print("---- batch ----")
-			print("gt_graph: ", gt_graph)
-			print("set_feature: ", set_feature)
-			
-			print("in_graph.shape: ", in_graph.shape)
-			print("gt_graph.shape: ", gt_graph.shape)
-			print("set_feature.shape: ", set_feature.shape)
-			
-			nxGraphs = darwin_batches_to_networkx_graphs(gt_graph, set_feature)
-			return nxGraphs
-			
-			'''
-			graphs_tuple = utils_np.networkxs_to_graphs_tuple(nxGraphs)
-			print("graphs_tuple.shape : ", graphs_tuple.map(lambda a: a if a is None else a.shape, fields=graphs.ALL_FIELDS))
-			saveable_string = graphs_tuple_dumps(graphs_tuple)
-			with open("surf/surf{}.json".format(epoch+1), 'w') as file:
-				file.write(saveable_string)
-			break
-			'''
+def generate_raw_graphs(rand, batch_size, min_max_nodes, geo_density):  
+    num_nodes = rand.random_integers(*min_max_nodes)
+    surface_type = str(rand.choice(SURFACE_TYPES, 1)[0])
+    gen_graph = GenerateDataGraphSurface(type_dataset=surface_type, num_surfaces=batch_size, num_points=num_nodes)
+    epochs=1
+    for epoch in range(epochs):
+        gen_trainig = gen_graph.train_generator( batch_size = batch_size )
+        counter = 0
+        for gt_graph, set_feature, in_graph in gen_trainig:
+            print ("gt_graph.shape = ", gt_graph.shape)
+            print ("set_feature.shape = ", set_feature.shape)
+            print ("surface_type = ", surface_type)
+            nxGraphs = darwin_batches_to_networkx_graphs(gt_graph, set_feature, surface_type)
+            return nxGraphs
+
+#@title Converters to/from Darwin's format  { form-width: "20%" }
+from graph_nets import blocks
+from graph_nets import graphs
+from graph_nets import modules
+from graph_nets import utils_np
+from graph_nets import utils_tf
+
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+import sonnet as snt
+import tensorflow as tf
+
+def darwin_batches_to_networkx_graphs(graphs, node_features, surface_type):
+    '''
+    Args:
+        graph : adjacency matrix of the graph, shape = (num_graphs, num_nodes, num_nodes)
+        node_features :  Matrix of node features, shape = (num_graphs, num_nodes, num_node_features)
+
+    Returns:
+        graphs_tuple : GraphTuple from graph_nets
+    '''
+    global SURFACE_TYPES
+    nxGraphs = []
+    for graph, node_feature in zip(graphs, node_features):
+        nxGraph = nx.from_numpy_matrix(graph, create_using=nx.DiGraph)
+        nx.set_node_attributes(G = nxGraph, name ="pos", values = {n : val for n,val in enumerate(node_feature)})
+        nx.set_edge_attributes(G = nxGraph, name ="distance", values = {
+            (u,v) : np.linalg.norm(node_feature[u] - node_feature[v])
+            for (u,v) in nxGraph.edges
+        })
+        nxGraph.graph['type'] = to_one_hot(SURFACE_TYPES.index(surface_type), len(SURFACE_TYPES))
+        nxGraphs.append(nxGraph)
+
+    return nxGraphs
+
+import json
+def json_default(obj) :
+    class_name = obj.__class__.__name__
+    serialization = {
+        'int64' : int,
+        'int32' : int,
+        'ndarray' : list
+    }
+    if class_name in serialization:
+        return serialization[class_name](obj)
+    else:
+        print("Unserializable object {} of type {}".format(obj, type(obj)))
+        raise TypeError(
+            "Unserializable object {} of type {}".format(obj, class_name)
+        )
+
+def json_dumps(obj, indent = 4, default = json_default) :
+    return json.dumps(obj, indent = indent, default = default)
+
+def graphs_tuple_dumps(graphs_tuple):
+    data_dicts = utils_np.graphs_tuple_to_data_dicts(graphs_tuple)
+    return json_dumps(data_dicts)
+
+def graphs_tuple_loads(string_dump):
+    data_dicts = json.loads(string_dump)
+    for data_dict in data_dicts:
+        for key in data_dict:
+            data_dict[key] = np.array(data_dict[key])
+
+    graphs_tuple = utils_np.data_dicts_to_graphs_tuple(data_dicts)
+    return graphs_tuple
+
+
+def graphs_tuples_to_darwin_batches(graph_nets):
+    '''
+    Args:
+        graphs_tuple : GraphTuple from graph_nets
+    Returns:
+        graph : adjacency matrix of the graph, shape = (num_graphs, num_nodes, num_nodes)
+        node_features :  Matrix of node features, shape = (num_graphs, num_nodes, num_node_features)
+    '''
+    adjs = []
+    node_features = []
+    data_dicts = utils_np.graphs_tuple_to_data_dicts(graph_nets)
+    for data_dict in data_dicts:
+        nodes = data_dict['nodes']
+        num_nodes= len(nodes)
+        adj = np.zeros(shape = (num_nodes, num_nodes))
+        senders = data_dict['senders']
+        receivers = data_dict['receivers']
+        adj[senders, receivers] = 1
+        adjs.append(adj)
+        node_features.append(nodes)
+
+    return np.array(adjs), np.array(node_features)
 
 #@title #Visualize example graphs  { form-width: "30%" }
 def rgb_from_hex(h):
@@ -168,12 +211,17 @@ def rgb_from_hex(h):
 seed = 5  #@param{type: 'integer'}
 rand = np.random.RandomState(seed=seed)
 
+num_examples = 4  #@param{type: 'integer'}
 
-num_examples = 2  #@param{type: 'integer'}
 
+min_nodes = 34 #@param {type:"slider", min:4, max:64, step:1}
+max_nodes = 36 #@param {type:"slider", min:4, max:64, step:1}
 
-min_nodes = 4 #@param {type:"slider", min:4, max:64, step:1}
-max_nodes = 5 #@param {type:"slider", min:4, max:64, step:1}
+theta = 12  #@param{type:"slider", min:4, max:64, step:1}
+#@markdown Large values (1000+) make trees. Try 20-60 for good non-trees.
+
+horizontal_length = 20 #@param{type: 'integer'}
+graphs_per_column = 2 #@param{type: 'integer'}
 
 min_max_nodes = (min_nodes, max_nodes)
 
@@ -361,267 +409,38 @@ def create_feed_dict(
     rand,
     batch_size,
     min_max_nodes,
-    geo_density,
-    source_ph,
-    target_ph
-):
-    """Creates placeholders for the model training and evaluation.
+    theta
+)
 
-    Args:
-        rand: A random seed (np.RandomState instance).
-        batch_size: Total number of graphs per batch.
-        min_max_nodes: A 2-tuple with the [lower, upper) number of nodes per
-            graph. The number of nodes for a graph is uniformly sampled within this
-            range.
-        geo_density: A `float` threshold parameters for the geographic threshold graph's
-            threshold. Default= the number of nodes.
-        source_ph: The source graph's placeholders, as a graph namedtuple.
-        target_ph: The target graph's placeholders, as a graph namedtuple.
-
-    Returns:
-        feed_dict: The feed `dict` of source and target placeholders and data.
-        raw_graphs: The `dict` of raw networkx graphs.
-    """
-    sources, targets, raw_graphs = generate_networkx_graphs(
-        rand,
-        batch_size,
-        min_max_nodes,
-    )
-    source_graphs = utils_np.networkxs_to_graphs_tuple(sources)
-    target_graphs = utils_np.networkxs_to_graphs_tuple(targets)
-    feed_dict = {source_ph: source_graphs, target_ph: target_graphs}
-    return feed_dict, raw_graphs
-
-def compute_accuracy(target, output, use_nodes=False, use_edges=True):
-    """Calculate model accuracy.
-
-    Returns the number of correctly predicted shortest path nodes and the number
-    of completely solved graphs (100% correct predictions).
-
-    Args:
-        target: A `graphs.GraphsTuple` that contains the target graph.
-        output: A `graphs.GraphsTuple` that contains the output graph.
-        use_nodes: A `bool` indicator of whether to compute node accuracy or not.
-        use_edges: A `bool` indicator of whether to compute edge accuracy or not.
-
-    Returns:
-        correct: A `float` fraction of correctly labeled nodes/edges.
-        solved: A `float` fraction of graphs that are completely correctly labeled.
-
-    Raises:
-        ValueError: Nodes or edges (or both) must be used
-    """
-    if not use_nodes and not use_edges:
-        raise ValueError("Nodes or edges (or both) must be used")
-    tdds = utils_np.graphs_tuple_to_data_dicts(target)
-    odds = utils_np.graphs_tuple_to_data_dicts(output)
-    cs = []
-    ss = []
-    for td, od in zip(tdds, odds):
-        xn = np.argmax(td["nodes"], axis=-1)
-        yn = np.argmax(od["nodes"], axis=-1)
-        xe = np.argmax(td["edges"], axis=-1)
-        ye = np.argmax(od["edges"], axis=-1)
-        c = []
-        if use_nodes:
-            c.append(xn == yn)
-        if use_edges:
-            c.append(xe == ye)
-        c = np.concatenate(c, axis=0)
-        s = np.all(c)
-        cs.append(c)
-        ss.append(s)
-    correct = np.mean(np.concatenate(cs, axis=0))
-    solved = np.mean(np.stack(ss))
-    return correct, solved
-
-#@title Run training  { form-width: "30%" }
-
-# You can interrupt this cell's training loop at any time, and visualize the
-# intermediate results by running the next cell (below). You can then resume
-# training by simply executing this cell again.
-
-# How much time between logging and printing the current results.
-log_every_seconds = 20
-
-print("# (iteration number), T (elapsed seconds), "
-            "Ltr (training loss), Lge (test/generalization loss), "
-            "Ctr (training fraction nodes/edges labeled correctly), "
-            "Str (training fraction examples solved correctly), "
-            "Cge (test/generalization fraction nodes/edges labeled correctly), "
-            "Sge (test/generalization fraction examples solved correctly)")
-
-start_time = time.time()
-last_log_time = start_time
-for iteration in range(last_iteration, num_training_iterations):
-    last_iteration = iteration
-    feed_dict, _ = create_feed_dict(
-        rand,
-        batch_size_tr,
-        num_nodes_min_max_tr,
-        theta,
-        input_ph,
-        target_ph
-    )
-    train_values = sess.run({
-            "step": step_op,
-            "target": target_ph,
-            "loss": loss_op_tr,
-            "outputs": output_ops_tr
-    },
-                                                    feed_dict=feed_dict)
-    the_time = time.time()
-    elapsed_since_last_log = the_time - last_log_time
-    if elapsed_since_last_log > log_every_seconds:
-        last_log_time = the_time
-        feed_dict, raw_graphs = create_feed_dict(
-                rand, batch_size_ge, num_nodes_min_max_ge, theta, input_ph, target_ph)
-        test_values = sess.run(
-            {
-                "target": target_ph,
-                "loss": loss_op_ge,
-                "outputs": output_ops_ge
-            },
-            feed_dict=feed_dict
-        )
-        correct_tr, solved_tr = compute_accuracy(
-            train_values["target"],
-            train_values["outputs"][-1],
-            use_edges=True
-        )
-        correct_ge, solved_ge = compute_accuracy(
-            test_values["target"],
-            test_values["outputs"][-1],
-            use_edges=True
-        )
-        elapsed = time.time() - start_time
-        losses_tr.append(train_values["loss"])
-        corrects_tr.append(correct_tr)
-        solveds_tr.append(solved_tr)
-        losses_ge.append(test_values["loss"])
-        corrects_ge.append(correct_ge)
-        solveds_ge.append(solved_ge)
-        logged_iterations.append(iteration)
-        print(
-            "# {:05d}, T {:.1f}, Ltr {:.4f}, Lge {:.4f}, Ctr {:.4f}, Str"
-            " {:.4f}, Cge {:.4f}, Sge {:.4f}".format(
-                iteration, elapsed, train_values["loss"], test_values["loss"],
-                correct_tr, solved_tr, correct_ge, solved_ge
-            )
-        )
-#@title Visualize results  { form-width: "30%" }
-
-# This cell visualizes the results of training. You can visualize the
-# intermediate results by interrupting execution of the cell above, and running
-# this cell. You can then resume training by simply executing the above cell
-# again.
-
-def softmax_prob_last_dim(x):  # pylint: disable=redefined-outer-name
-    e = np.exp(x)
-    return e[:, -1] / np.sum(e, axis=-1)
-
-
-# Plot results curves.
-fig = plt.figure(1, figsize=(18, 3))
+num = min(num_examples, 16)
+size = horizontal_length/graphs_per_column
+w = graphs_per_column
+h = int(np.ceil(num / w))
+fig = plt.figure(40, figsize=(w * size, h * size))
 fig.clf()
-x = np.array(logged_iterations)
-# Loss.
-y_tr = losses_tr
-y_ge = losses_ge
-ax = fig.add_subplot(1, 3, 1)
-ax.plot(x, y_tr, "k", label="Training")
-ax.plot(x, y_ge, "k--", label="Test/generalization")
-ax.set_title("Loss across training")
-ax.set_xlabel("Training iteration")
-ax.set_ylabel("Loss (binary cross-entropy)")
-ax.legend()
-# Correct.
-y_tr = corrects_tr
-y_ge = corrects_ge
-ax = fig.add_subplot(1, 3, 2)
-ax.plot(x, y_tr, "k", label="Training")
-ax.plot(x, y_ge, "k--", label="Test/generalization")
-ax.set_title("Fraction correct across training")
-ax.set_xlabel("Training iteration")
-ax.set_ylabel("Fraction nodes/edges correct")
-# Solved.
-y_tr = solveds_tr
-y_ge = solveds_ge
-ax = fig.add_subplot(1, 3, 3)
-ax.plot(x, y_tr, "k", label="Training")
-ax.plot(x, y_ge, "k--", label="Test/generalization")
-ax.set_title("Fraction solved across training")
-ax.set_xlabel("Training iteration")
-ax.set_ylabel("Fraction examples solved")
+for j, graph in enumerate(graphs):
+    ax = fig.add_subplot(h, w, j + 1, projection='3d')
+    points_coord_dict = nx.get_node_attributes(graph,'pos')
+    points_coord = []
+    for u in points_coord_dict:
+        points_coord.append(points_coord_dict[len(points_coord)])
+    points_coord = np.array(points_coord)                   
+    print(points_coord)
+    x = points_coord[:,0]
+    y = points_coord[:,1]
+    z = points_coord[:,2]
 
-# Plot graphs and results after each processing step.
-# The white node is the start, and the black is the end. Other nodes are colored
-# from red to purple to blue, where red means the model is confident the node is
-# off the shortest path, blue means the model is confident the node is on the
-# shortest path, and purplish colors mean the model isn't sure.
-max_graphs_to_plot = 8 #@param{type:"slider", min:4, max:8, step:1}
-num_steps_to_plot = 4 #@param{type:"slider", min:1, max:8, step:1}
-node_size = 120 #@param{type:"slider", min:64, max:2048, step:1}
-min_c = 0.3
-num_graphs = len(raw_graphs)
-targets = utils_np.graphs_tuple_to_data_dicts(test_values["target"])
-step_indices = np.floor(
-    np.linspace(
-        0, num_processing_steps_ge - 1,
-        num_steps_to_plot
-    )
-).astype(int).tolist()
-
-outputs = list(zip(*(
-    utils_np.graphs_tuple_to_data_dicts(test_values["outputs"][i])
-    for i in step_indices
-)))
-h = min(num_graphs, max_graphs_to_plot)
-w = num_steps_to_plot + 1
-fig = plt.figure(101, figsize=(18, h * 3))
-fig.clf()
-ncs = []
-for j, (graph, target, output) in enumerate(zip(raw_graphs, targets, outputs)):
-    if j >= h:
-        break
-    ground_truth = target["nodes"][:, -1]
-    # Ground truth.
-    iax = j * (1 + num_steps_to_plot) + 1
-    ax = fig.add_subplot(h, w, iax)
-    plotter = GraphPlotter(ax, graph)
-    color = {}
-    for i, n in enumerate(plotter.nodes):
-        color[n] = np.array(
-            [1.0 - ground_truth[i], 0.0, ground_truth[i], 1.0]
-        ) * (1.0 - min_c) + min_c
-    plotter.draw_graph_with_solution(node_size=node_size, node_color=color)
-    ax.set_axis_on()
-    ax.set_xticks([])
-    ax.set_yticks([])
-    try:
-        ax.set_facecolor([0.9] * 3 + [1.0])
-    except AttributeError:
-        ax.set_axis_bgcolor([0.9] * 3 + [1.0])
-    ax.grid(None)
-    ax.set_title(
-        "Ground truth\nSolution length: {}"
-            .format(plotter.solution_length)
-    )
-    # Prediction.
-    for k, outp in enumerate(output):
-        iax = j * (1 + num_steps_to_plot) + 2 + k
-        ax = fig.add_subplot(h, w, iax)
-        plotter = GraphPlotter(ax, graph)
-        color = {}
-        prob = softmax_prob_last_dim(outp["nodes"])
-        for i, n in enumerate(plotter.nodes):
-            color[n] = np.array(
-                [1.0 - prob[n], 0.0, prob[n], 1.0]
-            ) * (1.0 - min_c) + min_c
-        plotter.draw_graph_with_solution(node_size=node_size, node_color=color)
-        ax.set_title(
-            "Model-predicted\nStep {:02d} / {:02d}".format(
-                step_indices[k] + 1,
-                step_indices[-1] + 1
-            )
+    list_edges = []
+    #plot lines from edges
+    for u,v in graph.edges:
+        line = plt3d.art3d.Line3D(
+            [x[u],x[v]], 
+            [y[u],y[v]], 
+            [z[u],z[v]], 
+            linewidth=0.4, 
+            c="black", 
+            alpha=1.
         )
+        ax.add_line(line)
+
+    ax.scatter(x,y,z, marker='.', s=15, c="blue", alpha=0.6)
